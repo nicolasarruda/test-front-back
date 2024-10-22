16fb389d320f2c9a581133e974fb715e183754e9
nicolasarruda <nlarruda@hotmail.com>
2024-10-22 07:49:35 -0300

    Back ok

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,35 +1,49 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const { 
+  getAllStudents, 
+  addNewStudent, 
+  getStudentDetail, 
+  setStudentStatus, 
+  updateStudent 
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { studentId,name, email, role, systemAcess, lastlogin } = req.query;
+    const students = await getAllStudents({ studentId,name, email, role, systemAcess, lastlogin });
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id: studentId } = req.params;
+    const payload = req.body;
+    const message = await updateStudent({ ...payload, studentId });
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id: studentId } = req.params;
+    const { status } = req.body;
+    const { id: reviewerId } = req.user;
+    const message = await setStudentStatus({ studentId, status, reviewerId });
+    res.json(message);
 });
 
 module.exports = {
     handleGetAllStudents,
-    handleGetStudentDetail,
     handleAddStudent,
-    handleStudentStatus,
     handleUpdateStudent,
+    handleGetStudentDetail,
+    handleStudentStatus,
 };
